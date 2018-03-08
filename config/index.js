413ae7f474089702ed694ebcb006bdb0e9f41404
oss92 <mohamed.o.alnagdy@gmail.com>
2018-03-08 15:53:09 +0100

    Env variables

--- config/index.js
+++ config/index.js
@@ -1,6 +1,9 @@
 module.exports = {
   awsRegion: process.env.AWS_REGION,
-  dynamoLoadWriteCapacity: parseInt(process.env.DYNAMO_LOAD_WRITE_CAPACITY, 10),
-  dynamoTable: process.env.DYNAMO_TABLE,
-  s3FileLocation: process.env.S3_FILE_LOCATION
+  dynamoLoadWriteCapacity: parseInt(process.env.WRITE_CAPACITY_DURING_LOAD, 10),
+  dynamoTable: process.env.TABLE_NAME,
+  dynamoTableEndpoint: process.env.TABLE_ENDPOINT, // for test env only
+  dynamoWriteCapacity: parseInt(process.env.WRITE_CAPACITY, 10),
+  s3Bucket: process.env.S3_BUCKET,
+  s3Prefix: process.env.S3_PREFIX
 };
