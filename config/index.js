6ef86eec4f3a26dc224751c9f0fcbdeefc5d249b
oss92 <mohamed.o.alnagdy@gmail.com>
2018-03-20 17:22:08 +0100

    Add wait times for DB scaling

--- config/index.js
+++ config/index.js
@@ -6,6 +6,7 @@ module.exports = {
   dynamoTableEndpoint: process.env.TABLE_ENDPOINT, // for test env only
   dynamoWriteCapacity: parseInt(process.env.WRITE_CAPACITY, 10),
   dynamoReadCapacity: parseInt(process.env.READ_CAPACITY, 10),
+  dynamoScaleWait: 5000,
   s3Bucket: process.env.S3_BUCKET,
   s3Prefix: process.env.S3_PREFIX,
 };
